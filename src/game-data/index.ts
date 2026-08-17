d25f3fa583c99f55089988a71ffafc6b75097501
M4STER-3 <ghrjmtab01@gmail.com>
2026-08-17 10:27:25 +0200

    export game database foundation

---
+++ src/game-data/index.ts
@@ -0,0 +1,23 @@
+export { createGameCatalog } from "./catalog";
+export type { GameCatalog, GameDataDiagnostic } from "./catalog";
+export type {
+  CharacterCatalogEntry,
+  EchoCatalogEntry,
+  EchoCost,
+  EchoStatRollDefinition,
+  EchoStatTableCatalog,
+  ExternalIdMap,
+  GameDatabaseManifest,
+  GameDatabaseV1,
+  GameDataProvider,
+  GameEntityIdentity,
+  GameEntityKind,
+  GeneratedSourceMetadata,
+  NumericStatProgression,
+  NumericStatProgressionPoint,
+  PassiveRank,
+  SequenceCatalogEntry,
+  SkillCatalogEntry,
+  SonataSetCatalogEntry,
+  WeaponCatalogEntry,
+} from "./schema";
