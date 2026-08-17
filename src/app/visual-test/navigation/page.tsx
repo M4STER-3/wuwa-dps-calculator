30387de50498b2027039d02d04dd67444b92e49b
M4STER-3 <ghrjmtab01@gmail.com>
2026-08-18 01:40:44 +0200

    add navigation overlay visual test route

---
+++ src/app/visual-test/navigation/page.tsx
@@ -0,0 +1,10 @@
+import { ArtworkNavigation } from "@/components/v2/artwork-navigation";
+import { GlobalBackgroundStage } from "@/components/v2/global-background-stage";
+
+export default function NavigationVisualTestPage() {
+  return (
+    <GlobalBackgroundStage>
+      <ArtworkNavigation />
+    </GlobalBackgroundStage>
+  );
+}
